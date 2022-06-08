9f5cea33cc5b4c950b02aaff7a88d575b04b276a
seonghbae <fullbloomh3ck@gmail.com>
2022-06-08 10:29:03 +0900

    [추가] 0608 react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
+  return <header><h1><a href="/">Web</a></h1></header>
+}
+
+function Nav(props) {
+  const list = props.data.map((e) => {
+    return <li key={e.id}><a href={'/read/'+e.id}>{e.title}</a></li>
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <ol>
+        {list}
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'},
+  ];
+  return (
+    <div>
+      <Header></Header>
+      <Nav data={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
     </div>
   );
 }
