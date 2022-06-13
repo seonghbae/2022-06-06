538b2e04084bcc4f5f270674027fb55a3798e5ee
seonghbae <fullbloomh3ck@gmail.com>
2022-06-13 14:49:23 +0900

    0613 create, delete 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import {useState} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import styled from 'styled-components';
 
 function Header(props) {
-  const myStyle = {
-    borderBottom: '10px solid grey',
-    padding: '10px',
-    fontSize: '20px'
-  }
-  return <header style={myStyle}><h1><a href="/" onClick={(evt) => {
+  return <header><h1><a href="/" onClick={(evt) => {
     evt.preventDefault();
     props.onSelect();
   }}>Web</a></h1></header>
 }
+
+const HeaderStyled = styled(Header)`
+  border-bottom: 1px solid grey;
+`;
 
 function Nav(props) {
   const liTags = props.data.map((e) => {
@@ -39,14 +39,31 @@ function Article(props) {
   </article>
 }
 
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(evt) => {
+      evt.preventDefault(); // 페이지 리프레시 방지
+      const title = evt.target.title.value;
+      const body = evt.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"></input></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>;
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME'); // 새로 만들어진 상태의 default value
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'},
-  ];
+  ]);
   let content = null;
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB!"></Article>;
@@ -59,12 +76,21 @@ function App() {
       }
     })[0];
     content = <Article title={topic.title} body={topic.body}></Article>;
+  } else if(mode ==='CREATE') {
+    content = <Create onCreate={(title, body) => {
+      const newTopic = {id:nextId, title, body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setNextId(nextId + 1);
+    }}></Create>;
   }
   return (
     <div>
-      <Header onSelect={() => {
+      <HeaderStyled onSelect={() => {
         setMode('WELCOME');
-      }}></Header>
+      }}></HeaderStyled>
       <Nav data={topics} onSelect={(id) => {
         setMode('READ');
         setId(id);
@@ -72,11 +98,21 @@ function App() {
       {content}
       <ButtonGroup>
         <Button variant="outlined" onClick={() => {
-          alert('create!');
+          setMode('CREATE');
         }}>Create</Button>
         <Button variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button variant="outlined">Delete</Button>
+      <Button variant="outlined" onClick={() => {
+        const newTopics = topics.filter((e) => {
+          if(e.id === id) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+        setMode('WELCOME'); // 삭제한 글의 내용을 읽을 수 없기 때문에 mode 변경
+        setTopics(newTopics);
+      }}>Delete</Button>
     </div>
   );
 }
