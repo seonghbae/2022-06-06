6991a0f923400eecc443122b5a34ae045b467368
seonghbae <fullbloomh3ck@gmail.com>
2022-06-15 14:17:12 +0900

    0615 useEffect로 사이드이펙트 처리, ajax로 글목록 가져오기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import styled from 'styled-components';
-import { Link, Routes, Route, useParams } from 'react-router-dom';
+import { Link, Routes, Route, useParams, useNavigate } from 'react-router-dom';
 
 function Header(props) {
   return <header><h1><Link to="/" onClick={(evt) => {
-    // evt.preventDefault();
     props.onSelect();
   }}>Web</Link></h1></header>
 }
@@ -21,7 +20,6 @@ const HeaderStyled = styled(Header)`
 function Nav(props) {
   const liTags = props.data.map((e) => {
     return <li key={e.id}><Link to={'/read/'+e.id} onClick={(evt) => {
-      // evt.preventDefault(); // 클릭 시 a태그 발동하지 않도록
       props.onSelect(e.id); // e는 클로저로 인해 사용? e는 map함수로 정의되어 접근 가능(클로저)
     }}>{e.title}</Link></li>
   });
@@ -57,15 +55,52 @@ function Create(props) {
   </article>;
 }
 
+function Read(props) {
+  const params = useParams();
+  const id = Number(params.topic_id);
+  const topic = props.topics.filter(e => {
+    if(e.id === id) {
+      return true;
+    } else {
+      return false;
+    }
+  })[0];
+  return <Article title={topic.title} body={topic.body}></Article>;
+}
+
+function Control(props) {
+  const params = useParams();
+  const id = Number(params.topic_id);
+  let contextUI = null;
+  if(id) {
+    contextUI = <>
+      <Button variant="outlined">Update</Button>
+      <Button variant="outlined" onClick={() => {props.onDelete(id);}}>Delete</Button>
+    </>
+  }
+  return <>
+    <Button component={Link} to="/create" variant="outlined">Create</Button>
+    {contextUI}
+  </>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME'); // 새로 만들어진 상태의 default value todo 삭제 예정
   const [id, setId] = useState(null); // todo 삭제 예정
-  const [nextId, setNextId] = useState(4);
+  const [nextId, setNextId] = useState(3);
   const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'js', body:'js is ...'},
   ]);
+  useEffect(() => {
+    // side effect 작업
+    (async () => {
+      const response = await fetch('http://localhost:3333/topics');
+      const data = await response.json();
+      setTopics(data);
+    })();
+  }, []);
+  const navigate = useNavigate();
   return (
     <div>
       <HeaderStyled onSelect={headerHandler()}></HeaderStyled>
@@ -75,26 +110,15 @@ function App() {
         <Route path="/create" element={<Create onCreate={onCreateHandler()}></Create>}></Route>
         <Route path="/read/:topic_id" element={<Read topics={topics}></Read>}></Route>
       </Routes>
-      <ButtonGroup>
-        <Button component={Link} to="/create" variant="outlined" onClick={createHandler()}>Create</Button>
-        <Button variant="outlined">Update</Button>
-      </ButtonGroup>
-      <Button variant="outlined" onClick={deleteHandler()}>Delete</Button>
+      <Routes>
+        {['/', '/read/:topic_id', '/update/:topic_id'].map(path => {
+          return <Route key={path} path={path} element={<Control onDelete={(id) => {
+            deleteHandler(id);
+          }}></Control>}></Route>
+        })}
+      </Routes>
     </div>
   );
-
-  function Read(props) {
-    const params = useParams();
-    const id = Number(params.topic_id);
-    const topic = props.topics.filter(e => {
-      if(e.id === id) {
-        return true;
-      } else {
-        return false;
-      }
-    })[0];
-    return <Article title={topic.title} body={topic.body}></Article>;
-  }
 
   function onCreateHandler() {
     return (title, body) => {
@@ -102,6 +126,7 @@ function App() {
       const newTopics = [...topics];
       newTopics.push(newTopic);
       setTopics(newTopics);
+      setId(nextId);
       setMode('READ');
       setNextId(nextId + 1);
     };
@@ -114,18 +139,16 @@ function App() {
     };
   }
 
-  function deleteHandler() {
-    return () => {
-      const newTopics = topics.filter((e) => {
-        if (e.id === id) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-      setMode('WELCOME'); // 삭제한 글의 내용을 읽을 수 없기 때문에 mode 변경
-      setTopics(newTopics);
-    };
+  function deleteHandler(id) { 
+    const newTopics = topics.filter((e) => {
+      if (e.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setTopics(newTopics);
+    navigate('/'); // 해당 주소로 리프레시없이 보내줌
   }
 
   function createHandler() {
